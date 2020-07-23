484fa2fbf4c23428c65ca98e0ba37c67232295f9
Qize <louisliu1206@yahoo.com>
2020-07-23 03:13:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ function nowServing(katzDeli) {
   }
 }
 
-var array = [];
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    for (let i = 0; i < katzDeliLine.length; i++) {
-      array.push(` ${i + 1}. ${katzDeliLine[i]}`);
-    }
+function currentLine(line) {
+  if (line.length === 0) {
+    return `The line is currently empty.`;
   } else {
-  return "The line is currently empty.";
-}
-return `The line is currently:` + array;
+
+    var array = [];
+    for (var i = 0; i < line.length; i++) {
+      array.push(` ${i+1}. ${line[i]}`);
+    }
+    return `The line is currently:` + array;
+  }
 }
